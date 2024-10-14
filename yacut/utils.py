d9422e4ba36c401bf5a39718684e41b5547ff379
Igor Kolomytsev <igor.kolomitseff@yandex.ru>
2024-10-14 16:52:07 +0300

    Fix utils.py

--- yacut/utils.py
+++ yacut/utils.py
@@ -2,7 +2,7 @@ from random import choices
 
 from .models import URLMap
 from settings import (
-    SHORT_ID_BY_FUNC_MAX_LENGTH,
+    SHORT_ID_BY_FUNCTION_MAX_LENGTH,
     VALID_CHARACTERS_FOR_SHORT_ID
 )
 
@@ -15,7 +15,7 @@ def get_unique_short_id():
     while True:
         short_id = ''.join(choices(
             VALID_CHARACTERS_FOR_SHORT_ID,
-            k=SHORT_ID_BY_FUNC_MAX_LENGTH
+            k=SHORT_ID_BY_FUNCTION_MAX_LENGTH
         ))
         if not is_short_id_present(short_id):
             return short_id
